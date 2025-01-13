cf4a40940b691eea8dee86f683a702161f2d6c03
basharatali112 <basharat62200150@gmail.com>
2025-01-13 23:50:23 +0500

    FOOTER + ROUTING

--- src/App.jsx
+++ src/App.jsx
@@ -1,55 +1,28 @@
 import React from 'react'
 
+import { BrowserRouter, Route, Routes } from 'react-router-dom'
+import Body from './Page/Body/Body'
+import Login from './Components/Login/Login'
+import SignUp from './Components/SignUp/SignUp'
+
 const App = () => {
   return (
     <>
-    <div className="navbar bg-base-300">
-    <div className="flex-1">
-      <a className="btn btn-ghost text-xl">devAffinity</a>
-    </div>
-    <div className="flex-none gap-2">
+    <BrowserRouter basename='/'>
+    <Routes>
+      <Route path='/' element={<Body/>}>
       
-      <div className="dropdown dropdown-end">
-        <div tabIndex={0} role="button" className="btn btn-ghost btn-circle avatar">
-          <div className="w-10 rounded-full">
-            <img
-              alt="Tailwind CSS Navbar component"
-              src="https://img.daisyui.com/images/stock/photo-1534528741775-53994a69daeb.webp" />
-          </div>
-        </div>
-        <ul
-          tabIndex={0}
-          className="menu menu-sm dropdown-content bg-base-100 rounded-box z-[1] mt-3 w-52 p-2 shadow mr-50">
-          <li>
-            <a className="justify-between">
-              Profile
-              <span className="badge">New</span>
-            </a>
-          </li>
-          <li><a>Settings</a></li>
-          <li><a>Logout</a></li>
-        </ul>
-      </div>
-    </div>
-  </div>
-  <div className="card bg-base-100 w-96 shadow-xl mt-7 ml-6">
-  <figure>
-    <img
-      src="https://img.daisyui.com/images/stock/photo-1606107557195-0e29a4b5b4aa.webp"
-      alt="Shoes" />
-  </figure>
-  <div className="card-body">
-    <h2 className="card-title">
-      Shoes!
-      <div className="badge badge-secondary">NEW</div>
-    </h2>
-    <p>If a dog chews shoes whose shoes does he choose?</p>
-    <div className="card-actions justify-end">
-      <div className="badge badge-outline">Fashion</div>
-      <div className="badge badge-outline">Products</div>
-    </div>
-  </div>
-</div>
+      <Route path='/login' element={<Login/>}></Route>
+      <Route path='/signup' element={<SignUp/>}></Route>
+      </Route>
+     
+
+    </Routes>
+
+
+  </BrowserRouter>
+
+
   </>
   )
 }
---
+++ src/README.md
@@ -0,0 +1,11 @@
+# devAffinity 
+
+-Create a Vite + React application 
+-Remove unnecessary code and create a Hello World app
+-Install Tailwind Css
+-Add NavBAr component to App.jsx
+-create a NavBar.jsx separate Component file 
+-Install react router dom
+-Create BrowserRouter > Routes> Route =/Body > RouteChildren
+-Create an Outlet in your Body Component 
+-Create a Footer
