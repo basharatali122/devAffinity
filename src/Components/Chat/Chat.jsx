c5fbb3a4c46400eb0b18457ed376379e4378ad5f
basharatali112 <basharat62200150@gmail.com>
2025-04-23 13:07:37 +0500

    messageSend recive emit

--- src/Components/Chat/Chat.jsx
+++ src/Components/Chat/Chat.jsx
@@ -2,6 +2,8 @@ import React, { useEffect, useState } from "react";
 import { useSelector } from "react-redux";
 import { useParams } from "react-router-dom";
 import { createSocketConnection } from "../../Utils/socket";
+import axios from "axios";
+import { BASE_URL } from "../../Utils/constants";
 
 const Chat = () => {
   const { requestId } = useParams();
@@ -9,6 +11,29 @@ const Chat = () => {
   const [newMessage, setNewMessage] = useState("");
   const user = useSelector((state) => state.user);
   const userId = user?._id;
+
+  const fetchChatMessages = async () => {
+    const chat = await axios.get(BASE_URL + "/chat/" + requestId, {
+      withCredentials: true,
+    });
+
+    console.log(chat.data.message);
+
+    const chatMessages = chat?.data?.messages.map((msg) => {
+
+      const { senderId, text } = msg;
+      return {
+        firstName: senderId?.firstName,
+        lastName: senderId?.lastName,
+        text,
+      };
+    });
+    setMessage(chatMessages)
+  };
+
+  useEffect(() => {
+    fetchChatMessages();
+  }, []);
 
   useEffect(() => {
     if (!userId || !requestId) {
@@ -19,12 +44,11 @@ const Chat = () => {
 
     socket.emit("joinChat", { firstName: user.firstName, userId, requestId });
 
-    socket.on("messageReceived",({firstName,text})=>{
+    socket.on("messageReceived", ({ firstName,lastName, text }) => {
+      console.log(firstName + " " + text);
 
-      console.log(firstName+ " "+ text)
-
-      setMessage((message)=>[...message,{firstName,text}])
-    })
+      setMessage((message) => [...message, { firstName, lastName, text }]);
+    });
 
     return () => {
       socket.disconnect();
@@ -35,11 +59,12 @@ const Chat = () => {
     const socket = createSocketConnection();
     socket.emit("sendMessage", {
       firstName: user.firstName,
+      lastName:user.lastName,
       userId,
       requestId,
       text: newMessage,
     });
-    setNewMessage("")
+    setNewMessage("");
   };
 
   return (
@@ -52,16 +77,15 @@ const Chat = () => {
         {message.map((msg, index) => {
           return (
             <React.Fragment key={index}>
-              <div className="chat chat-start">
+              <div className={"chat " + (user.firstName === msg.firstName ? "chat-end" : "chat-start")}>
                 <div className="chat-header">
-                {msg.firstName}
+                  {`${msg.firstName} ${msg.lastName}`}
                   <time className="text-xs opacity-50">2 hours ago</time>
                 </div>
                 <div>{msg.text}</div>
                 <div className="chat-bubble">{msg.text}</div>
                 <div className="chat-footer opacity-50">Seen</div>
               </div>
-             
             </React.Fragment>
           );
         })}
@@ -70,9 +94,7 @@ const Chat = () => {
       <div className="p-5 border-t border-gray-600 flex items-center gap-2">
         <input
           value={newMessage}
-          onChange={(e) => setNewMessage(e.target.value)
-           
-          }
+          onChange={(e) => setNewMessage(e.target.value)}
           type="text"
           className="flex-1 border border-gray-400 rounded py-2 text-white"
           placeholder="Type your message..."
